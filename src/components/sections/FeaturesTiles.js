bdac22bf5f192a566c80fbebfff9cfd1e81dc5e3
mojjoiv <daltonongeche@gmail.com>
2020-08-12 11:51:14 +0300

    new push

--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -152,7 +152,7 @@ const FeaturesTiles = ({
                 <div className="features-tiles-item-header">
                   <div className="features-tiles-item-image mb-16">
                     <Image
-                      src={require('./../../assets/images/feature-tile-icon-05.svg')}
+                      src={require('./../../assets/images/undraw_two_factor_authentication_namy.svg')}
                       alt="Features tile icon 05"
                       width={64}
                       height={64} />
@@ -160,10 +160,10 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Robust Workflow
+                    Authentication
                     </h4>
                   <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                    Authentication services to built apps and new apps.
                     </p>
                 </div>
               </div>
@@ -174,7 +174,7 @@ const FeaturesTiles = ({
                 <div className="features-tiles-item-header">
                   <div className="features-tiles-item-image mb-16">
                     <Image
-                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
+                      src={require('./../../assets/images/undraw_chat_1wo5.svg')}
                       alt="Features tile icon 06"
                       width={64}
                       height={64} />
@@ -182,10 +182,10 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Robust Workflow
+                    Online Chat systems
                     </h4>
                   <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
+                    We do online chat rooms and virtual assistants for websites
                     </p>
                 </div>
               </div>
